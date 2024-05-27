4109356c00f93efc850ec4ac6c49393ac1e5e4d6
AlexLissmann <alexlissmann@web.de>
2024-05-27 23:23:02 +0200

    eig nichts neues

--- To-Do-Liste/main.js
+++ To-Do-Liste/main.js
@@ -23,9 +23,19 @@ function Elementehinzufuegen(html) {
     return template.content.firstElementChild;
 }
 // 
-const EingabeFeld = Elementehinzufuegen('<input placeholder="Aufgabe" class="TextEingabe" class="inline" type="text">')
+const EingabeFeld = Elementehinzufuegen('<form id="myForm"><input placeholder="Aufgabe" class="TextEingabe" class="inline" type="text"></form><input type="submit" class="noshowbutton">')
 KreuzAufgabeHinzufuegen.addEventListener("click", ()=>{
 //    KreuzAufgabeHinzufuegen.remove();   
     KreuzAufgabeHinzufuegen.replaceWith(EingabeFeld);
 })
 
+document.getElementsByClassName("TextEingabe")[0].addEventListener("keypress", (event)=>{
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+//        document.getElementsByClassName("noshowbutton")[0].click();
+        let inputWert = document.getElementsByClassName("TextEingabe").value;
+        console.log("Eingabe:", inputWert);
+      }
+})
