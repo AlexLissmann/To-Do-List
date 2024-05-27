c0e8b77904a123065353850dbf1985f96f045884
AlexLissmann <alexlissmann@web.de>
2024-05-27 17:48:15 +0200

    mit js

---
+++ To-Do-Liste/main.js
@@ -0,0 +1,31 @@
+// AufgabeHinzufügen Hover Bild ändern
+
+
+const KreuzAufgabeHinzufuegen = document.getElementsByClassName("AufgabeDazu")[0]; 
+KreuzAufgabeHinzufuegen.addEventListener("mouseover", ()=>{
+image = document.getElementsByClassName("KreuzAufgabeHinzufuegen")[0];
+image.src = "src/RotAufgabeHinzufuegenHover.png"
+text = document.querySelector("p").style.color = "red";
+
+})
+
+KreuzAufgabeHinzufuegen.addEventListener("mouseout", ()=>{
+image = document.getElementsByClassName("KreuzAufgabeHinzufuegen")[0];
+image.src = "src/KreuzAufgabehinzufuegen.png"
+text = document.querySelector("p").style.color = "black";
+})
+
+// Aufgabe Hinzufügen
+
+function Elementehinzufuegen(html) {
+    const template = document.createElement("template");
+    template.innerHTML = html.trim();
+    return template.content.firstElementChild;
+}
+// 
+KreuzAufgabeHinzufuegen.addEventListener("click", ()=>{
+//    KreuzAufgabeHinzufuegen.remove();
+    const EingabeFeld = Elementehinzufuegen('<input placeholder="Aufgabe" class="TextEingabe" class="inline" type="text">')
+    EingabeFeld.replaceChild(KreuzAufgabeHinzufuegen, EingabeFeld.childNodes)
+})
+
